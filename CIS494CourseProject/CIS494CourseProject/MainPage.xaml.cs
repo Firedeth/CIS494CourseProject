2652992ca39f0eea414c7d83bbf3346aa08bbeca
Firedeth <james042886@hotmail.com>
2018-10-01 20:06:51 -0400

    Add project files.

---
+++ CIS494CourseProject/CIS494CourseProject/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CIS494CourseProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
